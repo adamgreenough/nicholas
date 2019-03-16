ae35ab4977fe2b6bc4b93b850935b16ac6c09adb
Adam Greenough <adam@amazorize.com>
2019-03-16 23:02:51 +0000

    Better 404 page

--- views/404.php
+++ views/404.php
@@ -1,1 +1,53 @@
-Uh-oh, 404!
+<!doctype HTML>
+<html>
+<head>
+	<title>Error 404</title>
+	
+	<style>
+		* {
+		    transition: all 0.6s;
+		}
+		
+		html {
+		    height: 100%;
+		}
+		
+		body {
+		    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+		    color: #888;
+		    margin: 0;
+		}
+		
+		#main {
+		    display: table;
+		    width: 100%;
+		    height: 100vh;
+		    text-align: center;
+		}
+		
+		.error {
+			display: table-cell;
+			vertical-align: middle;
+		}
+		
+		.error h1 {
+			font-size: 50px;
+			display: inline-block;
+		}
+		
+		.error a {
+			color: #419cff;
+			font-weight: bold;
+			text-decoration: none;
+		}
+	</style>
+</head>
+<body>
+	<div id="main">
+		<div class="error">
+	    	<h1>Oops, Error 404</h1>
+	    	<p>Click <a href="/">here</a> to go back home.</p>
+		</div>
+	</div>
+</body>
+</html>
