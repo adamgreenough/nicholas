d136aa1eef6284747169aeb3ac7d71674438467b
Doug Nelson <dougnelson@silktide.com>
2019-03-17 20:02:04 +0000

    moved to use ?? for isset checks

--- app/api.php
+++ app/api.php
@@ -1,33 +1,13 @@
 <?php
 	
 function api_feed() {
-	if(isset($_GET['page'])) {
-		$page = $_GET['page'];
-	} else {
-		$page = 1;
-	}
-	
-	if(isset($_GET['perpage'])) {
-		$perPage = $_GET['perpage'];
-	} else {
-		$perPage = POSTS_PER_PAGE;
-	}
-	
-	if(isset($_GET['tag'])) {
-		$tag = $_GET['tag'];
-	} else {
-		$tag = null;
-	}
-	
+	$page = $_GET['page'] ?? 1;
+	$perPage = $_GET['perpage'] ?? POSTS_PER_PAGE;
+	$tag = $_GET['tag'] ?? null;
 	return get_posts($page, $perPage, $tag);
 }
 
 function api_single() {
-	if(isset($_GET['slug'])) {
-		$slug = $_GET['slug'];
-	} else {
-		$slug = null;
-	}
-	
+	$slut = $_GET['slug'] ?? null;
 	return get_single($slug);
 }
