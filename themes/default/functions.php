0843e60265bbbf673b415d11ac4a22f054044572
Adam Greenough <adam@greenough.me>
2020-03-08 17:39:08 +0000

    🥅 Catch tag list error

--- themes/default/functions.php
+++ themes/default/functions.php
@@ -2,6 +2,7 @@
 	
 function display_tag_list($tags) {
 	$config = include('config.php');
+	$output = '';
 	
 	foreach ($tags as $tag) {
 		$output .= 	'<a href="' . $config['base_url'] . '/tag/' . strtolower($tag) . '/" title="Posts tagged ' . $tag . '">' . $tag . '</a>, ';
