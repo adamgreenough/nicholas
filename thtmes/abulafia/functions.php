35e9648919c7b1c6ab39dc47274b9d3dea6f16eb
Moonstone <sejo.gaming.com@gmail.com>
2021-07-10 16:44:40 +0200

    uh bulk add theme i guess?

---
+++ thtmes/abulafia/functions.php
@@ -0,0 +1,12 @@
+<?php
+	
+function display_tag_list($tags) {
+	$config = include('config.php');
+	
+	foreach ($tags as $tag) {
+		$output .= 	'<a href="' . $config['base_url'] . '/tag/' . strtolower($tag) . '/" title="Posts tagged ' . $tag . '">' . $tag . '</a>, ';
+	}
+	
+	$output = rtrim($output, ', ');
+	return $output;
+}
